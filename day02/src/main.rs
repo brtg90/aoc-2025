85547f0e6c4bb0ea6c13daeef758a709e3be1ffb
brtg90 <bartgroeneveld1@hotmail.com>
2025-12-02 12:10:52 +0100

    Day02 pt 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use utils::read_input;
 
 #[derive(Debug)]
@@ -20,7 +22,11 @@ fn part_1() {
 }
 
 fn part_2() {
-    println!("Part 2: {:?}", 0);
+    let ranges = parse("inputs/day02pt1.txt");
+    let sum: usize = ranges.iter()
+        .map(|range| get_all_invalid_ids_from_range(range).iter().sum::<usize>())
+        .sum();
+    println!("Part 2: {:?}", sum);
 }
 
 fn get_invalid_double_ids_from_range(range: &Range) -> Vec<usize> {
@@ -29,6 +35,39 @@ fn get_invalid_double_ids_from_range(range: &Range) -> Vec<usize> {
         .filter(|s| {
             let len = s.len();
             len % 2 == 0 && s[0..len / 2] == s[len / 2..]
+        })
+        .map(|s| s.parse().unwrap())
+        .collect()
+}
+
+fn get_all_invalid_ids_from_range(range: &Range) -> Vec<usize> {
+    (range.start..=range.end)
+        .map(|i| i.to_string())
+        .filter(|s| {
+            let mut unique_chars: HashMap<char, usize> = HashMap::new();
+            s.chars().
+                for_each(|c| {
+                    unique_chars.entry(c)
+                    .and_modify(|v| *v += 1)
+                    .or_insert(1);
+                });
+            let min_repeats = unique_chars.values().min().unwrap();
+
+            if *min_repeats == 1 {
+                return false
+            }
+
+            // The code below finds anything based on chunks consisting of length s.len() / min_repeats
+            // This fails for edge cases where the min_repeat number is included twice into the
+            // duplicated part, e.g. 12211221. To include these, simply look at the old method
+            if s[0..s.len() / 2] == s[s.len() / 2..] {
+                return true;
+            }
+
+            let chars = s.chars().collect::<Vec<char>>();
+            let chunks = chars.chunks(s.len() / min_repeats).collect::<Vec<&[char]>>();
+            chunks.iter()
+                .all(|ch| ch == &chunks[0])
         })
         .map(|s| s.parse().unwrap())
         .collect()
