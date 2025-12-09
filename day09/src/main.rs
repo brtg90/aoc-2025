91f93fa7d8fd6a09e142ab09aff520d3d6f9f8f1
brtg90 <bartgroeneveld1@hotmail.com>
2025-12-09 08:25:01 +0100

    Day09 pt 1

---
+++ day09/src/main.rs
@@ -0,0 +1,47 @@
+use utils::read_lines;
+
+fn main() {
+    part_1();
+    part_2();
+}
+
+fn part_1() {
+    let grid = parse("inputs/day09pt1.txt");
+    let tiles = find_maximum_tiles_between_points(&grid);
+    println!("Part 1: {:?}", tiles);
+}
+
+fn part_2() {
+    println!("Part 2: {:?}", 0);
+}
+
+fn find_maximum_tiles_between_points(grid: &[(isize, isize)]) -> isize {
+    let mut corner_0 = (0, 0);
+    let mut corner_1 = (0, 0);
+
+    let mut max = 0;
+
+    for (i, p0) in grid.iter().enumerate() {
+        for p1 in grid.iter().skip(i + 1) {
+            let tiles = ((p0.0 - p1.0).abs() + 1) * ((p0.1 - p1.1).abs() + 1);
+            if tiles > max {
+                max = tiles;
+                corner_0 = *p0;
+                corner_1 = *p1;
+            }
+        }
+    }
+    println!("Corners: {:?} {:?}", corner_0, corner_1);
+    max
+}
+
+fn parse(filename: &str) -> Vec<(isize, isize)> {
+    read_lines(filename)
+        .into_iter()
+        .map(|line| {
+            let mut iter = line.split(",")
+                .map(|str| str.parse::<isize>().unwrap());
+            (iter.next().unwrap(), iter.next().unwrap())
+        })
+    .collect()
+}
