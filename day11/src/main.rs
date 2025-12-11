663300c18ee951532946c06cc94003408eefa430
brtg90 <bartgroeneveld1@hotmail.com>
2025-12-11 14:19:48 +0100

    Day11 pt 2

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -9,26 +9,51 @@ fn main() {
 
 fn part_1() {
     let devices = parse("inputs/day11pt1.txt");
-    let total_paths = count_paths(&devices, String::from("you"));
+    let mut cache: HashMap<String, usize> = HashMap::new();
+    let total_paths = count_paths(&devices, String::from("you"), "out", &mut cache);
     println!("Part 1: {:?}", total_paths);
 }
 
 fn part_2() {
-    println!("Part 2: {:?}", 0);
+    let devices = parse("inputs/day11pt1.txt");
+    let mut cache: HashMap<String, usize> = HashMap::new();
+    // Both orders could be possible, and we don't know which one is correct
+    let first = count_paths(&devices, String::from("svr"), "fft", &mut cache);
+    cache.clear();
+    let second = count_paths(&devices, String::from("fft"), "dac", &mut cache);
+    cache.clear();
+    let third = count_paths(&devices, String::from("dac"), "out", &mut cache);
+    let mut total_paths = first * second * third;
+
+    cache.clear();
+    let first = count_paths(&devices, String::from("svr"), "dac", &mut cache);
+    cache.clear();
+    let second = count_paths(&devices, String::from("dac"), "fft", &mut cache);
+    cache.clear();
+    let third = count_paths(&devices, String::from("fft"), "out", &mut cache);
+    total_paths += first * second * third;
+    println!("Part 2: {:?}", total_paths);
 }
 
-fn count_paths(devices: &HashMap<String, Vec<String>>, current: String) -> usize {
+fn count_paths(devices: &HashMap<String, Vec<String>>, current: String, end: &str, cache: &mut HashMap<String, usize>) -> usize {
     let mut paths = 0;
 
-    if current == String::from("out") {
+    if current == end {
         paths += 1;
+    }
+
+    if let Some(cached) = cache.get(&current) {
+        return *cached;
     }
 
     if let Some(options) = devices.get(&current) {
         for option in options {
-            paths += count_paths(devices, option.to_string());
+            paths += count_paths(devices, option.to_string(), end, cache);
         }
     }
+
+    cache.insert(current, paths);
+
     paths
 }
 
